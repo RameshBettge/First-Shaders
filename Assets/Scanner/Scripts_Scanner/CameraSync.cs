01dab018642ca26b56c3eedd623ad7a58e2b4b5f
RameshBettge <bettge.r@gmx.de>
2018-09-05 14:47:00 +0200

    Created: Scanner-scene, scanner shaders.

---
+++ Assets/Scanner/Scripts_Scanner/CameraSync.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSync : MonoBehaviour
+{
+    [SerializeField]
+    Transform secondCam;
+
+    [SerializeField]
+    float speed = 1f;
+
+    Vector3 offset = Vector3.right * 1000f;
+
+    void Start()
+    {
+        offset -= transform.position;
+    }
+
+    void LateUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        if (h != 0f)
+        {
+            transform.Rotate(Vector3.up * h);
+        }
+        else
+        {
+            transform.Rotate(-Vector3.right * Input.GetAxis("Vertical"));
+        }
+
+        secondCam.position = transform.position + offset;
+        secondCam.rotation = transform.rotation;
+    }
+}
